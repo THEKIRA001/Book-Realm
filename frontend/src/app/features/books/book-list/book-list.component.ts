0a87d64f3b571eb984522d9ee54f5e13e6bdc7f4
Shubham Bolave <111694017+THEKIRA001@users.noreply.github.com>
2025-12-08 21:31:50 +0530

    Added Pagination to Books List

--- frontend/src/app/features/books/book-list/book-list.component.ts
+++ frontend/src/app/features/books/book-list/book-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BooksStore } from '../../../core/stores/books.store';
 
@@ -13,7 +13,31 @@ import { BooksStore } from '../../../core/stores/books.store';
 })
 export class BookListComponent {
   readonly store = inject(BooksStore);
+
+  currentPage = signal(1);
+  pageSize = 8;
+  totalPages = computed(() => Math.ceil(this.store.allBooks().length / this.pageSize));
+  paginatedBooks = computed(() => {
+    const startIndex = (this.currentPage() - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.store.allBooks().slice(startIndex, endIndex);
+  });
+
   ngOnInit(){
     this.store.loadBooks();
+  }
+
+  nextPage() {
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update(p => p + 1);
+      window.scrollTo(0, 0);
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(p => p - 1);
+      window.scrollTo(0, 0);
+    }
   }
 }
